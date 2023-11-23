c7142a58a2410e40d698268076080127d6ded7e4
E910ir <142308364+E910ir@users.noreply.github.com>
2023-11-23 21:10:32 +0900

    Score_Chapter04の修正版を追加

--- Score_Chapter04.java
+++ Score_Chapter04.java
@@ -3,17 +3,17 @@ package kadai_004;
 public class Score_Chapter04 {
 
 	public static void main(String[] args) {
-		System.out.println("Aさん" + 10 + "点");
-		System.out.println("Bさん" + 20 + "点");
-		System.out.println("Cさん" + 30 + "点");
-		System.out.println("Dさん" + 40 + "点");
-		System.out.println("Eさん" + 50 + "点");
-		System.out.println("Fさん" + 60 + "点");
-		System.out.println("Gさん" + 70 + "点");
-		System.out.println("Hさん" + 80 + "点");
-		System.out.println("Iさん" + 90 + "点");
-		System.out.println("Jさん" + 100 + "点");
-		System.out.println((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10);
+		System.out.println("Aさんの点数は" + 10 + "点");
+		System.out.println("Bさんの点数は" + 20 + "点");
+		System.out.println("Cさんの点数は" + 30 + "点");
+		System.out.println("Dさんの点数は" + 40 + "点");
+		System.out.println("Eさんの点数は" + 50 + "点");
+		System.out.println("Fさんの点数は" + 60 + "点");
+		System.out.println("Gさんの点数は" + 70 + "点");
+		System.out.println("Hさんの点数は" + 80 + "点");
+		System.out.println("Iさんの点数は" + 90 + "点");
+		System.out.println("Jさんの点数は" + 100 + "点");
+		System.out.println("テストの平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10 + "点");
 		
 	}
 
