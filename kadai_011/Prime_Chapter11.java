36eda1dda26dff9869e345665808fc6371b2e5d2
E910ir <142308364+E910ir@users.noreply.github.com>
2023-12-21 22:20:10 +0900

    kadai_011を追加

---
+++ kadai_011/Prime_Chapter11.java
@@ -0,0 +1,32 @@
+package kadai_011;
+
+public class Prime_Chapter11 {
+	
+	public static void main(String[] args) {
+		
+		boolean[] isPrime = new boolean[101];
+		
+		for( int i = 2; i < isPrime.length; i++ ) {
+			isPrime[i] = true;
+		}
+		
+		for( int i = 2; i < isPrime.length; i++ ) {
+			
+			if( isPrime[i] ) {
+				
+				for ( int j = 2 * i; j < isPrime.length; j += i ) {
+					
+					isPrime[j] = false;
+				}
+			}
+		}
+		
+		for( int i = 2; i < isPrime.length; i++ ) {
+			if( isPrime[i] ) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
